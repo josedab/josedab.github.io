af372799b1ca6bcd9923a7845c36eba2655e2404
José David Baena <josedab@github.com>
2019-07-21 19:28:45 +0200

    Initial configuration changes for personal website - Configuration update with personal branch changes

--- content/meta/config.js
+++ content/meta/config.js
@@ -1,23 +1,23 @@
 module.exports = {
   siteTitle: "HeroBlog - a GatsbyJS starter", // <title>
   shortSiteTitle: "HeroBlog - GatsbyJS starter", // <title> ending for posts and pages
-  siteDescription: "HeroBlog is a GatsbyJS starter.",
-  siteUrl: "https://gatsby-starter-hero-blog.greglobinski.com",
+  siteDescription: "José David Baena personal website.",
+  siteUrl: "https://josedab.github.io",
   // pathPrefix: "",
   siteImage: "preview.jpg",
   siteLanguage: "en",
 
   /* author */
-  authorName: "greg lobinski",
-  authorTwitterAccount: "greglobinski",
+  authorName: "José David Baena",
+  authorTwitterAccount: "josedab",
 
   /* info */
-  headerTitle: "greg lobinski",
-  headerSubTitle: "presents another one GatsbyJS starter",
+  headerTitle: "José David Baena",
+  headerSubTitle: "Work, thoughts and general nonsense",
 
   /* manifest.json */
-  manifestName: "HeroBlog - a GatsbyJS starter",
-  manifestShortName: "HeroBlog", // max 12 characters
+  manifestName: "José David Baena personal website",
+  manifestShortName: "Jose Baena", // max 12 characters
   manifestStartUrl: "/index.html",
   manifestBackgroundColor: "white",
   manifestThemeColor: "#666",
@@ -32,8 +32,8 @@ module.exports = {
 
   // social
   authorSocialLinks: [
-    { name: "github", url: "https://github.com/greglobinski" },
-    { name: "twitter", url: "https://twitter.com/greglobinski" },
-    { name: "facebook", url: "http://facebook.com/greglobinski" }
+    { name: "github", url: "https://github.com/josedab" },
+    { name: "twitter", url: "https://twitter.com/josedab" },
+    { name: "facebook", url: "https://www.facebook.com/josedab" }
   ]
 };
--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -10,7 +10,7 @@ const Hero = props => {
     <React.Fragment>
       <section className="hero">
         <h1>
-          This is a demo site of&nbsp;the <strong>heroBlog</strong> GatsbyJS starter
+          Howdy! My name is <strong>José David Baena</strong>. Nice to meet you!
         </h1>
         <button onClick={scrollToContent} aria-label="scroll">
           <FaArrowDown />
